58dfdcd2ed99c40108927337f4a31dc478927ebd
Yashwanth D <118149969+14240@users.noreply.github.com>
2025-06-15 22:18:15 +0530

    Update clang_linux_test_input.c
    Added return statement

--- clang_linux_test_input.c
+++ clang_linux_test_input.c
@@ -1,4 +1,8 @@
 // This file is used to check if we can produce working executables
 // for i386 and x86_64 archs on Linux.
 #include <stdlib.h>
-int main(){}
+using namespace std;
+
+int main(){
+  return 0;
+}
