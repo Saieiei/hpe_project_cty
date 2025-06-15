22538ebe196293ec1360868ce7cd03bf09923363
Yashwanth D <118149969+14240@users.noreply.github.com>
2025-06-15 22:18:54 +0530

    Update clang_linux_test_input.c

--- clang_linux_test_input.c
+++ clang_linux_test_input.c
@@ -1,8 +1,4 @@
 // This file is used to check if we can produce working executables
 // for i386 and x86_64 archs on Linux.
 #include <stdlib.h>
-using namespace std;
-
-int main(){
-  return 0;
-}
+int main(){}
